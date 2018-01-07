e7b55bf0e116e757adec6d1b12fe5bc6dfbe2dc4
Dale Lane <dale.lane@uk.ibm.com>
2018-01-07 00:54:08 +0000

    fix: Stop webcam reporting open
    On Chrome, even after the webcam dialog closes, the browser still
    reports that the webcam is on.
    
    Sending the destroy event is a way to get the angular directive to
    call the video stop() method.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- public/components/training/training.controller.js
+++ public/components/training/training.controller.js
@@ -341,9 +341,11 @@
                     $scope.webcamCanvas = null;
 
                     $scope.hide = function() {
+                        closeWebcam()
                         $mdDialog.hide();
                     };
                     $scope.cancel = function() {
+                        closeWebcam()
                         $mdDialog.cancel();
                     };
                     $scope.confirm = function() {
@@ -359,6 +361,21 @@
                             $scope.webcamInitComplete = true;
                         });
                     };
+
+                    function closeWebcam() {
+                        if ($scope.channel &&
+                            $scope.channel.video)
+                        {
+                            try {
+                                var closeWebcam = new Event('$destroy');
+                                $scope.channel.video.dispatchEvent(closeWebcam);
+                            }
+                            catch (err) {
+                                console.log('Failed to stop webcam');
+                                console.log(err);
+                            }
+                        }
+                    }
 
                     function displayWebcamError(err) {
                         $scope.webcamerror = err;
@@ -399,6 +416,8 @@
                         ctx.drawImage($scope.channel.video,
                             0, 0,
                             $scope.channel.video.width, $scope.channel.video.height);
+
+                        closeWebcam()
 
                         return $q(function(resolve, reject) {
                             hiddenCanvas.toBlob(function (blob) {
