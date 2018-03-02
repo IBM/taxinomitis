178d612c72070e6c26435d1315e010bc9334c900
Dale Lane <dale.lane@uk.ibm.com>
2018-03-02 22:51:14 +0000

    fix: Email template support for HTML viewers
    Replaced angle-brackets in templates with underscores instead.
    
    Closes: #84
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- public/components/login/login.controller.js
+++ public/components/login/login.controller.js
@@ -16,7 +16,7 @@
         readFocusFromUrl();
 
         vm.sendEmail = function () {
-            window.location = 'mailto:dale.lane@uk.ibm.com?subject=New%20MLforKids%20class%20account&body=Please%20can%20you%20setup%20a%20new%20class%20account%20for%20my%20group.%20%0A%0AI%20need%20it%20from%20<THIS%20DATE>%20to%20<THAT%20DATE>%20for%20my%20group%20of%20<THIS%20MANY>%20students.%20%0A%0AI%20run%20<DESCRIPTION%20OF%20CODING%20GROUP%20AND%20THE%20STUDENTS%20I%20WORK%20WITH>.%0A%0AThanks%20very%20much!%0A%0A<WHO%20I%20AM>';
+            window.location = 'mailto:dale.lane@uk.ibm.com?subject=New%20MLforKids%20class%20account&body=___PLEASE%20FILL%20IN%20THIS%20TEMPLATE___%0A%0APlease%20can%20you%20setup%20a%20new%20class%20account%20for%20my%20group.%20%0A%0AI%20need%20it%20from%20___THIS%20DATE___%20to%20___THAT%20DATE___%20for%20my%20group%20of%20___THIS%20MANY___%20students.%20%0A%0AI%20run%20___DESCRIPTION%20OF%20CODING%20GROUP%20AND%20THE%20STUDENTS%20I%20WORK%20WITH___.%0A%0AThanks%20very%20much!%0A%0A___WHO%20I%20AM___';
         };
 
 
