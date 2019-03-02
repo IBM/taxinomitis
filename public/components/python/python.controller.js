033dc95db0815a143a1284864f4ff71adc5ac7ba
Dale Lane <dale.lane@uk.ibm.com>
2019-03-02 01:17:24 +0000

    feat: Generate Python for adding training examples to text projects
    Contributes to: #149
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- public/components/python/python.controller.js
+++ public/components/python/python.controller.js
@@ -17,16 +17,23 @@
             $scope.projectId = $stateParams.projectId;
             $scope.userId = $stateParams.userId;
 
+            $scope.functionType = 'classify';
+
             $scope.testsource = 'local';
             $scope.testdata = {
                 text      : 'The text that you want to test',
+                storetext : 'The text that you want to store',
                 imagefile : 'my-test-image.jpg',
                 imageurl  : 'https://www.site-on-the-internet.com/image.jpg',
-                fields : []
+                fields    : [],
+                label     : 'label'
             };
 
             $scope.setSource = function (source) {
                 $scope.testsource = source;
+            };
+            $scope.setFunctionType = function (type) {
+                $scope.functionType = type;
             };
 
             authService.getProfileDeferred()
@@ -37,6 +44,9 @@
                 })
                 .then(function (project) {
                     $scope.project = project;
+                    if (project.labels && project.labels.length > 0) {
+                        $scope.testdata.label = project.labels[0];
+                    }
 
                     if (project.type === 'numbers') {
                         return projectsService.getFields($scope.projectId, $scope.userId, vm.profile.tenant);
@@ -53,6 +63,10 @@
                 .then(function (resp) {
                     if (resp) {
                         $scope.scratchkey = resp[0];
+
+                        if (!$scope.scratchkey.model && $scope.project.type === 'text') {
+                            $scope.functionType = 'store';
+                        }
                     }
                 })
                 .catch(function (err) {
