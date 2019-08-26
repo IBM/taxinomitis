9f642a180bc24d864ca1ce598783167e55b53b07
Dale Lane <dale.lane@uk.ibm.com>
2019-08-26 20:44:31 +0100

    chore: Make it more obvious that fake credentials are fake
    The placeholder credentials used in pull request unit tests earned
    me persistent weekend evening messages to my phone from a panicky
    security monkey.
    
    Changing them to something more obviously fake that still meets the
    right length and character set will hopefully avoid future panics.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- sql/tables.sql
+++ sql/tables.sql
@@ -99,8 +99,10 @@ CREATE INDEX bluemixcredentials_getBluemixCredentials on bluemixcredentials(clas
 
 INSERT INTO bluemixcredentials (id, classid, servicetype, url, username, password)
     VALUES
-    ('01cf0343-6bd7-4732-95f0-8c8dbc0be922', 'testing', 'conv', 'https://gateway.watsonplatform.net/conversation/api', '263913bf-07b0-4d04-b08d-1a3a38c287d6', 'A8PV7mFeg37b'),
-    ('3a9a71ab-de77-4912-b0b0-f0c9698d9245', 'testing', 'visrec', 'https://gateway.watsonplatform.net/visual-recognition/api', 'ChfzmB5KwqQhu4jk0zKpOj', 'JqZfa6xuDgwcOmUoYHCBzE');
+    -- This is a placeholder row used for unit tests - it's obviously not a real username/password.
+    ('01cf0343-6bd7-4732-95f0-8c8dbc0be922', 'testing', 'conv', 'https://gateway.watsonplatform.net/conversation/api', '00000000-1111-2222-3333-444444444444', '56789abcdef0'),
+    -- This is a placeholder row used for unit tests - it's obviously not a real API key.
+    ('3a9a71ab-de77-4912-b0b0-f0c9698d9245', 'testing', 'visrec', 'https://gateway.watsonplatform.net/visual-recognition/api', 'AbCdEfGhIjKlMnOpQrStUv', 'WxYz0123456789AbCdEfGh');
 
 -- ------------------------------------------------------------------
 
