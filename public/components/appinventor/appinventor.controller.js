05d140b2787070a2774f4c996e75cc2add518df8
Dale Lane <dale.lane@uk.ibm.com>
2019-09-11 14:40:40 +0100

    feat: Use App Inventor to train ML models
    Now that the App Inventor extension has additional blocks
    for collecting training data and training models, there
    is no need to restrict the App Inventor extension to
    projects with a trained model.
    
    This commit reworks the App Inventor launch page to
    provide options for projects still in progress.
    
    Closes: #247
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- public/components/appinventor/appinventor.controller.js
+++ public/components/appinventor/appinventor.controller.js
@@ -17,6 +17,12 @@
         $scope.projectId = $stateParams.projectId;
         $scope.userId = $stateParams.userId;
 
+        $scope.projecturls = {
+            appinventor : 'http://ai2.appinventor.mit.edu/',
+            train : '/#!/mlproject/' + $stateParams.userId + '/' + $stateParams.projectId + '/training',
+            learnandtest : '/#!/mlproject/' + $stateParams.userId + '/' + $stateParams.projectId + '/models'
+        };
+
         authService.getProfileDeferred()
             .then(function (profile) {
                 vm.profile = profile;
@@ -25,6 +31,9 @@
             })
             .then(function (project) {
                 $scope.project = project;
+
+                $scope.projecturls.train = '/#!/mlproject/' + $scope.project.userid + '/' + $scope.project.id + '/training';
+                $scope.projecturls.learnandtest = '/#!/mlproject/' + $scope.project.userid + '/' + $scope.project.id + '/models';
 
                 return scratchkeysService.getScratchKeys($stateParams.projectId, $stateParams.userId, vm.profile.tenant);
             })
