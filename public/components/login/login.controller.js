cecb57165733cf3e1d4e04c67aaf94b014fe878c
Dale Lane <dale.lane@uk.ibm.com>
2019-10-09 09:14:46 +0100

    docs: Clarify information needed for managed accounts
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- public/components/login/login.controller.js
+++ public/components/login/login.controller.js
@@ -17,7 +17,7 @@
         readFocusFromUrl();
 
         vm.sendEmail = function () {
-            window.location = 'mailto:dale.lane@uk.ibm.com?subject=New%20MLforKids%20class%20account&body=___PLEASE%20FILL%20IN%20THIS%20TEMPLATE___%0A%0APlease%20can%20you%20setup%20a%20new%20class%20account%20for%20my%20group.%20%0A%0AI%20need%20it%20for%20my%20group%20of%20___THIS%20MANY___%20students.%20%0A%0AI%20run%20___DESCRIPTION%20OF%20CODING%20GROUP%20AND%20THE%20STUDENTS%20I%20WORK%20WITH___.%0A%0AThanks%20very%20much!%0A%0A___WHO%20I%20AM___';
+            window.location = 'mailto:dale.lane@uk.ibm.com?subject=New%20MLforKids%20class%20account&body=___PLEASE%20FILL%20IN%20THIS%20TEMPLATE___%0A%0APlease%20can%20you%20setup%20a%20new%20class%20account%20for%20my%20group.%20%0A%0AI%20need%20it%20for%20my%20group%20of%20___THIS%20MANY___%20students.%20%0A%0AI%20run%20___NAME%20OF%20SCHOOL%20%2F%20DESCRIPTION%20OF%20CODING%20GROUP___.%20%0A%0AYou%20can%20find%20us%20at%20___WEB%20ADDRESS%20FOR%20SCHOOL%20OR%20CODING%20GROUP___.%0A%0AThanks%20very%20much!%0A%0A___WHO%20I%20AM___';
         };
 
         vm.deployment = $stateParams.DEPLOYMENT;
