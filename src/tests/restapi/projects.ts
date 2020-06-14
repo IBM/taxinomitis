IBM/taxinomitis

commit 2fa7551a8ce7ffb2c305d9160743287ca30666d7

feat: Support for teachers reviewing training data
This commit is an update to the Teacher -> Supervision page, so
that the list of student projects is now a list of links to the
training page for each project. The supervision training page is
a read-only view, to allow teachers to review the training data
not contribute to it.

Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

src/lib/restapi/images/downloads.ts
@@ -19,7 +19,7 @@ export default function registerApis(app: Express.Application) {
     app.get(urls.IMAGE,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectAccess, // makes sure that the project exists
+            auth.verifyProjectOwnerOrTeacher,
             handleDownload);
 }
 

src/lib/restapi/projects.ts
@@ -425,7 +425,7 @@ export default function registerApis(app: Express.Application) {
     app.get(urls.PROJECT,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectAccess,
+            auth.verifyProjectOwnerOrTeacher,
             // @ts-ignore
             getProject);
 

src/lib/restapi/sounds/downloads.ts
@@ -11,7 +11,7 @@ import * as headers from '../headers';
 
 
 /**
- * Sets up API required to allow image downloads.
+ * Sets up API required to allow sound downloads.
  */
 export default function registerApis(app: Express.Application) {
 
@@ -19,7 +19,7 @@ export default function registerApis(app: Express.Application) {
     app.get(urls.SOUND,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectAccess, // makes sure that the project exists
+            auth.verifyProjectOwnerOrTeacher,
             handleDownload);
 }
 

src/lib/restapi/training.ts
@@ -211,14 +211,14 @@ export default function registerApis(app: Express.Application) {
     app.get(urls.TRAININGITEMS,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectAccess,
+            auth.verifyProjectOwnerOrTeacher,
             // @ts-ignore
             getTraining);
 
     app.get(urls.LABELS,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectAccess,
+            auth.verifyProjectOwnerOrTeacher,
             // @ts-ignore
             getLabels);
 

src/tests/restapi/training.ts
@@ -1103,6 +1103,100 @@ describe('REST API - training', () => {
                 });
         });
 
+        it('should ensure access is prevented to other students', async () => {
+            const classid = uuid();
+            const userid = uuid();
+
+            const project = await store.storeProject(userid, classid, 'text', 'demo', 'en', [], false);
+            const projectid = project.id;
+
+            const data = [];
+
+            for (let labelIdx = 0; labelIdx < 2; labelIdx++) {
+                const label = uuid();
+
+                for (let text = 0; text < 3; text++) {
+                    const textdata = uuid();
+
+                    data.push({ textdata, label });
+                }
+            }
+
+            await store.bulkStoreTextTraining(projectid, data);
+
+            nextAuth0UserId = userid;
+            nextAuth0UserTenant = classid;
+
+            const trainingurl = '/api/classes/' + classid + '/students/' + userid + '/projects/' + projectid + '/training';
+
+            return request(testServer)
+                .get(trainingurl)
+                .expect('Content-Type', /json/)
+                .expect(httpstatus.OK)
+                .then((res) => {
+                    const body: { id: string, label: string, textdata: string }[] = res.body;
+                    assert.strictEqual(body.length, 6);
+
+                    nextAuth0UserId = 'a-different-user';
+
+                    return request(testServer)
+                        .get(trainingurl)
+                        .expect('Content-Type', /json/)
+                        .expect(httpstatus.FORBIDDEN);
+                })
+                .then((res) => {
+                    assert.deepStrictEqual(res.body, { error : 'Invalid access' });
+
+                    return store.deleteEntireProject(userid, classid, project);
+                });
+        });
+
+        it('should ensure allow teachers to have read-only access to training data', async () => {
+            const classid = uuid();
+            const userid = uuid();
+
+            const project = await store.storeProject(userid, classid, 'text', 'demo', 'en', [], false);
+            const projectid = project.id;
+
+            const data = [];
+
+            for (let labelIdx = 0; labelIdx < 2; labelIdx++) {
+                const label = uuid();
+
+                for (let text = 0; text < 3; text++) {
+                    const textdata = uuid();
+
+                    data.push({ textdata, label });
+                }
+            }
+
+            await store.bulkStoreTextTraining(projectid, data);
+
+            nextAuth0UserId = userid;
+            nextAuth0UserTenant = classid;
+
+            const trainingurl = '/api/classes/' + classid + '/students/' + userid + '/projects/' + projectid + '/training';
+
+            return request(testServer)
+                .get(trainingurl)
+                .expect('Content-Type', /json/)
+                .expect(httpstatus.OK)
+                .then(() => {
+                    nextAuth0UserId = 'yet-another-different-user';
+                    nextAuth0UserRole = 'supervisor';
+
+                    return request(testServer)
+                        .get(trainingurl)
+                        .expect('Content-Type', /json/)
+                        .expect(httpstatus.OK);
+                })
+                .then((res) => {
+                    const body: { id: string, label: string, textdata: string }[] = res.body;
+                    assert.strictEqual(body.length, 6);
+
+                    return store.deleteEntireProject(userid, classid, project);
+                });
+        });
 
         it('should get a page of training', async () => {
             const classid = uuid();

public/app.js
@@ -106,6 +106,16 @@
                 templateUrl: 'static/components-<%= VERSION %>/teacher_supervision/teacher_supervision.html',
                 controllerAs: 'vm'
             })
+            .state('teacher_review_training', {
+                url: '/teacher/projects/:userId/:projectId/training',
+                controller: 'TrainingController',
+                templateUrl: 'static/components-<%= VERSION %>/training/training.html',
+                controllerAs: 'vm',
+                params: {
+                    review: true,
+                    VERSION : <%= VERSION %>
+                }
+            })
             .state('newproject', {
                 url: '/newproject',
                 controller: 'NewProjectController',
@@ -142,6 +152,7 @@
                 templateUrl: 'static/components-<%= VERSION %>/training/training.html',
                 controllerAs: 'vm',
                 params: {
+                    review: false,
                     VERSION : <%= VERSION %>
                 }
             })

public/components/teacher_supervision/teacher_supervision.html
@@ -105,7 +105,6 @@
             <p translate>TEACHER.SUPERVISION.NO_PROJECTS_1</p>
             <p translate>TEACHER.SUPERVISION.NO_PROJECTS_2</p>
         </div>
-
         <table class="studentslist wide" ng-if="vm.projects.length > 0">
             <tr>
                 <th title="Name of the project">
@@ -134,7 +133,12 @@
             </tr>
             <tr ng-repeat="project in vm.projects | orderBy : vm.orderBy" ng-class-even="'even'" ng-class-odd="'odd'">
                 <!-- Project name -->
-                <td class="projectslistname"> {{ project.name }} </td>
+                <td class="projectslistname" ng-hide="project.owner">{{ project.name }}</td>
+                <td class="projectslistname" ng-if="project.owner">
+                    <a style="cursor: pointer" ui-sref="teacher_review_training({ projectId : project.id, userId : project.owner.id })">
+                        {{ project.name }}
+                    </a>
+                </td>
                 <!-- Project type  (text/images/numbers) -->
                 <td class="studentslistdetail projecttype"> {{ project.type }} </td>
                 <!-- Classes / Labels -->

public/components/training/training.controller.js
@@ -54,6 +54,7 @@
         $scope.loadingtraining = true;
 
         $scope.crowdSourced = false;
+        $scope.reviewing = $stateParams.review;
 
         $scope.projectId = $stateParams.projectId;
         $scope.userId = $stateParams.userId;
@@ -418,7 +419,10 @@
 
         vm.deleteText = function (label, item, idx) {
             $scope.training[label].splice(idx, 1);
-            trainingService.deleteTrainingData($scope.projectId, $scope.userId, vm.profile.tenant, item.id);
+            trainingService.deleteTrainingData($scope.projectId, $scope.userId, vm.profile.tenant, item.id)
+                .catch(function (err) {
+                    displayAlert('errors', err.status, err.data);
+                });
         };
 
         vm.deleteLabel = function (ev, label, idx) {

commit afd34db45d9396d9615fc4c2d72109432129f898

fix: Fix project access
The changes in 2fa7551a8ce7ffb2c305d9160743287ca30666d7 broke some
of the crowd-sourced project access. This commit fixes that and
adds some tests to check for future regressions.

Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

src/lib/restapi/auth.ts
@@ -219,6 +219,9 @@ enum ACCESS_TYPE {
                    //   as long as the resource has been flagged as "crowd-sourced"
     teacheraccess, // access is allowed to the user who created the resource being accessed OR
                    //         their teacher
+    review,        // access is allowed to users in the same class as the creator of the resource being accessed
+                   //   as long as the resource has been flagged as "crowd-sourced", OR
+                   //   the teacher of the project owner
 }
 
 
@@ -261,15 +264,28 @@ async function verifyProjectAuth(
             //
             // That might be okay under some circumstances...
 
-            if (// owneronly : if they're not the owner, this access isn't allowed
-                (allowedAccessTypes === ACCESS_TYPE.owneronly) ||
-                // crowdsourced : if the project isn't crowd-sourced, this access isn't allowed
-                (allowedAccessTypes === ACCESS_TYPE.crowdsourced && !project.isCrowdSourced) ||
-                // teacheraccess : if the user isn't a teacher, this access isn't allowed
-                (allowedAccessTypes === ACCESS_TYPE.teacheraccess &&
-                 reqWithUser.user.app_metadata.role !== 'supervisor'))
-            {
+            // owneronly : if they're not the owner, this access isn't allowed
+            if (allowedAccessTypes === ACCESS_TYPE.owneronly) {
                 return errors.forbidden(res);
+            }
+            // crowdsourced : if the project isn't crowd-sourced, this access isn't allowed
+            if (allowedAccessTypes === ACCESS_TYPE.crowdsourced) {
+                if (!project.isCrowdSourced) {
+                    return errors.forbidden(res);
+                }
+            }
+            // teacheraccess : if the user isn't a teacher, this access isn't allowed
+            if (allowedAccessTypes === ACCESS_TYPE.teacheraccess) {
+                if (reqWithUser.user.app_metadata.role !== 'supervisor') {
+                    return errors.forbidden(res);
+                }
+            }
+            // review : if the project isn't crowd-sourced or the user isn't a teacher, this access isn't allowed
+            if (allowedAccessTypes === ACCESS_TYPE.review) {
+                if (reqWithUser.user.app_metadata.role !== 'supervisor' && !project.isCrowdSourced)
+                {
+                    return errors.forbidden(res);
+                }
             }
 
             // otherwise, carry on - it's okay
@@ -327,4 +343,19 @@ export async function verifyProjectOwnerOrTeacher(
     next: (e?: Error) => void)
 {
     verifyProjectAuth(req, res, next, ACCESS_TYPE.teacheraccess);
+}
+
+/**
+ * API Auth middleware.
+ *
+ * Ensures that the user is accessing a project that they
+ *  have at least read access to, or they are the teacher of
+ *  the owner of the project.
+ */
+export async function verifyProjectAccessOrTeacher(
+    req: Express.Request,
+    res: Express.Response,
+    next: (e?: Error) => void)
+{
+    verifyProjectAuth(req, res, next, ACCESS_TYPE.review);
 }

src/lib/restapi/images/downloads.ts
@@ -19,7 +19,7 @@ export default function registerApis(app: Express.Application) {
     app.get(urls.IMAGE,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectOwnerOrTeacher,
+            auth.verifyProjectAccessOrTeacher,
             handleDownload);
 }
 

src/lib/restapi/projects.ts
@@ -425,7 +425,7 @@ export default function registerApis(app: Express.Application) {
     app.get(urls.PROJECT,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectOwnerOrTeacher,
+            auth.verifyProjectAccessOrTeacher,
             // @ts-ignore
             getProject);
 

src/lib/restapi/sounds/downloads.ts
@@ -19,7 +19,7 @@ export default function registerApis(app: Express.Application) {
     app.get(urls.SOUND,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectOwnerOrTeacher,
+            auth.verifyProjectAccessOrTeacher,
             handleDownload);
 }
 

src/lib/restapi/training.ts
@@ -211,14 +211,14 @@ export default function registerApis(app: Express.Application) {
     app.get(urls.TRAININGITEMS,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectOwnerOrTeacher,
+            auth.verifyProjectAccessOrTeacher,
             // @ts-ignore
             getTraining);
 
     app.get(urls.LABELS,
             auth.authenticate,
             auth.checkValidUser,
-            auth.verifyProjectOwnerOrTeacher,
+            auth.verifyProjectAccessOrTeacher,
             // @ts-ignore
             getLabels);
 

src/tests/restapi/projects.ts
@@ -227,6 +227,69 @@ describe('REST API - projects', () => {
                 });
         });
 
+        it('should allow teachers to fetch non-crowd-sourced project for review', () => {
+            const studentId = uuid();
+            const teacherId = uuid();
+
+            nextAuth0UserTenant = TESTCLASS;
+
+            nextAuth0UserId = teacherId;
+            nextAuth0UserRole = 'student';
+
+            return request(testServer)
+                .post('/api/classes/' + TESTCLASS + '/students/' + studentId + '/projects')
+                .send({ name : uuid(), type : 'text', language : 'en', isCrowdSourced : false })
+                .expect('Content-Type', /json/)
+                .expect(httpstatus.CREATED)
+                .then((res) => {
+                    const body = res.body;
+                    assert(body.id);
+                    const projectId = body.id;
+
+                    nextAuth0UserId = teacherId;
+                    nextAuth0UserRole = 'supervisor';
+
+                    return request(testServer)
+                        .get('/api/classes/' + TESTCLASS + '/students/' + studentId + '/projects/' + projectId)
+                        .expect('Content-Type', /json/)
+                        .expect(httpstatus.OK);
+                });
+        });
+
+        it('should prevent students fetching non-crowd-sourced project for review', () => {
+            const studentId = uuid();
+            const teacherId = uuid();
+
+            nextAuth0UserTenant = TESTCLASS;
+
+            nextAuth0UserId = teacherId;
+            nextAuth0UserRole = 'student';
+
+            return request(testServer)
+                .post('/api/classes/' + TESTCLASS + '/students/' + studentId + '/projects')
+                .send({ name : uuid(), type : 'text', language : 'en', isCrowdSourced : false })
+                .expect('Content-Type', /json/)
+                .expect(httpstatus.CREATED)
+                .then((res) => {
+                    const body = res.body;
+                    assert(body.id);
+                    const projectId = body.id;
+
+                    nextAuth0UserId = teacherId;
+                    nextAuth0UserRole = 'student';
+
+                    return request(testServer)
+                        .get('/api/classes/' + TESTCLASS + '/students/' + studentId + '/projects/' + projectId)
+                        .expect('Content-Type', /json/)
+                        .expect(httpstatus.FORBIDDEN);
+                })
+                .then((res) => {
+                    const body = res.body;
+                    assert.deepStrictEqual(body, {
+                        error : 'Invalid access',
+                    });
+                });
+        });
     });
 
 