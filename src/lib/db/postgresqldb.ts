e4a8594b872881af93bc50790aba19e7b8c62b6f
Dale Lane <dale.lane@uk.ibm.com>
2020-08-09 00:47:56 +0100

    wip

--- src/lib/db/postgresqldb.ts
+++ src/lib/db/postgresqldb.ts
@@ -2,6 +2,7 @@
 import * as pg from 'pg';
 // local dependencies
 import * as env from '../utils/env';
+import * as fileutils from '../utils/fileutils';
 
 let connectionPool: pg.Pool | undefined;
 
@@ -9,17 +10,21 @@ export async function connect(): Promise<any> {
     if (!connectionPool) {
         pg.types.setTypeParser(20, parseInt);
 
-        connectionPool = new pg.Pool({
+        const connectionOptions: pg.PoolConfig = {
             host: process.env[env.POSTGRESQLHOST],
             port: parseInt(process.env[env.POSTGRESQLPORT] as string, 10),
             user: process.env[env.POSTGRESQLUSER],
             password: process.env[env.POSTGRESQLPASSWORD],
             database: process.env[env.POSTGRESQLDATABASE],
+        };
 
-            ssl: {
-                rejectUnauthorized: false
-            },
-        });
+        if (process.env[env.POSTGRESQLCERT]) {
+            connectionOptions.ssl = {
+                ca : await fileutils.read(process.env[env.POSTGRESQLCERT] as string),
+            };
+        }
+
+        connectionPool = new pg.Pool(connectionOptions);
     }
     return connectionPool;
 }
