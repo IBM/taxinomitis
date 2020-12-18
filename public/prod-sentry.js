368be9dafd1e72ad627a0f399dbc751dd1593521
Dale Lane <dale.lane@uk.ibm.com>
2020-12-18 16:44:40 +0000

    fix: Handle blocking of third-party scripts
    Browsers set to block third-party resources will prevent the
    Sentry script from being fetched, which means the global Sentry
    variable will be null. Adding this check stops this from crashing
    the rest of the page rendering.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- public/prod-sentry.js
+++ public/prod-sentry.js
@@ -1,3 +1,5 @@
-Sentry.init({
-    dsn: 'https://b4bbe1a8fbce473cb9eb089652848a1e@sentry.io/202347'
-});
+if (Sentry) {
+    Sentry.init({
+        dsn: 'https://b4bbe1a8fbce473cb9eb089652848a1e@sentry.io/202347'
+    });
+}
