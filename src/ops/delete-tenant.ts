9a1e2370f83c6b9d7d9c179dc6c429f08b53195b
Dale Lane <dale.lane@uk.ibm.com>
2021-01-23 18:07:43 +0000

    chore: Version bumps
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

---
+++ src/ops/delete-tenant.ts
@@ -0,0 +1,32 @@
+/* eslint no-console: 0 */
+/* tslint:disable: no-console max-line-length */
+import * as store from '../lib/db/store';
+import * as deleter from '../lib/classdeleter';
+
+const opsArgs = process.argv.slice(2);
+
+if (opsArgs.length !== 1) {
+    console.log('usage: node delete-tenant.js tenantid');
+    process.exit(-1); // eslint-disable-line
+}
+
+const tenantid  = opsArgs[0];
+
+console.log('tenant           :', tenantid);
+
+console.log('');
+console.log('connecting to DB...');
+store.init()
+    .then(() => {
+        console.log('deleting tenant record...');
+        return deleter.deleteClass(tenantid);
+    })
+    .then((newtenant) => {
+        console.log('deleted:', newtenant);
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+    .finally(() => {
+        return store.disconnect();
+    });
