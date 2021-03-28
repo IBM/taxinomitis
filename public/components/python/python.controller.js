89ae7e2e37594f3f366a1b41e68644d7dac53ab8
Dale Lane <dale.lane@uk.ibm.com>
2021-03-28 23:13:40 +0100

    docs: Make replit process more obvious
    Since replit.com's redesign, I'm getting a lot of student
    requests to edit the Python templates (instead of them making
    their own copy to edit).
    
    Making the requirement to click Fork more obvious by displaying
    it before opening the replit window.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- public/components/python/python.controller.js
+++ public/components/python/python.controller.js
@@ -6,10 +6,10 @@
 
         PythonController.$inject = [
             'authService', 'projectsService', 'scratchkeysService', 'loggerService',
-            '$mdDialog', '$stateParams', '$scope'
+            '$translate', '$mdDialog', '$stateParams', '$scope', '$window'
         ];
 
-        function PythonController(authService, projectsService, scratchkeysService, loggerService, $mdDialog, $stateParams, $scope) {
+        function PythonController(authService, projectsService, scratchkeysService, loggerService, $translate, $mdDialog, $stateParams, $scope, $window) {
 
             var vm = this;
             vm.authService = authService;
@@ -23,6 +23,9 @@
                 label     : 'label'
             };
 
+            var replitTitle = 'replit';
+            var replitTip = 'Click on Fork to start editing your own copy of the code';
+            var replitLaunch = 'Go';
 
             $scope.projectId = $stateParams.projectId;
             $scope.userId = $stateParams.userId;
@@ -66,12 +69,30 @@
                     };
                 });
 
-            $scope.openReplLink = function (ev) {
+            $translate([
+                'PYTHON.REPLIT.TITLE',
+                'PYTHON.REPLIT.TIP',
+                'PYTHON.REPLIT.GO'
+            ]).then(function (translations) {
+                replitTitle = translations['PYTHON.REPLIT.TITLE'];
+                replitTip = translations['PYTHON.REPLIT.TIP'];
+                replitLaunch = translations['PYTHON.REPLIT.GO'];
+            });
+
+            $scope.openReplLink = function (replurl, ev) {
                 $mdDialog.show($mdDialog.alert()
-                    .title('How to use repl.it')
-                    .textContent('Tip: Click on "Fork" to start editing your copy of the code')
+                    .title(replitTitle)
+                    .htmlContent(replitTip)
                     .targetEvent(ev)
-                    .ok('OK'));
+                    .ok(replitLaunch))
+                    .then(
+                        function () {
+                            $window.open(replurl, '_blank');
+                        },
+                        function() {
+                            // cancelled, do nothing
+                        }
+                    );
             };
         }
     }());
