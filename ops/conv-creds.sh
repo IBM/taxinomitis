aeea1ec5ed331b0a856bd41577552e5bc27f0711
Dale Lane <dale.lane@uk.ibm.com>
2021-03-29 09:36:20 +0100

    chore: Tidying
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- ops/conv-creds.sh
+++ ops/conv-creds.sh
@@ -21,17 +21,12 @@ ibmcloud resource service-key-create mlforkidsapikey Manager --instance-id "$con
 
 convapikey=`ibmcloud resource service-keys --instance-id "$convserviceid" --output json | jq --raw-output .[0].credentials.apikey`
 
-# uuid=`uuidgen | tr '[:upper:]' '[:lower:]'`
+uuid=`uuidgen | tr '[:upper:]' '[:lower:]'`
 
-# convuser=${convapikey:0:22}
-# convpass=${convapikey:22:22}
+convuser=${convapikey:0:22}
+convpass=${convapikey:22:22}
 
-# echo ""
-# echo ""
-# echo "INSERT INTO bluemixcredentials (id, classid, servicetype, credstypeid, url, username, password, notes) "
-# echo "VALUES "
-# echo "('$uuid', '$classname', 'conv', 2, 'https://gateway.watsonplatform.net/assistant/api', '$convuser', '$convpass', '$servicename')"
-# echo ";"
-
-echo $convapikey
-echo $servicename
+echo "INSERT INTO bluemixcredentials (id, classid, servicetype, credstypeid, url, username, password, notes) "
+echo "VALUES "
+echo "('$uuid', '$classname', 'conv', 2, 'https://gateway.watsonplatform.net/assistant/api', '$convuser', '$convpass', '$servicename')"
+echo ";"
