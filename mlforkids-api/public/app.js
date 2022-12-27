deb1a31dab5b5c5178b642847bf3c2c4d5d71d4a
Dale Lane <dale.lane@uk.ibm.com>
2022-12-27 21:04:37 +0000

    fix: fix broken JSON file
    I broke the fake-news.json file when I shuffled the lines.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -298,7 +298,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=56'
+                suffix: '.json?v=57'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
