c5f1c10f79b30c41008f0e1cc64d55dadb5cac4d
Dale Lane <dale.lane@uk.ibm.com>
2023-01-06 08:47:35 +0000

    docs: new dataset image
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -298,7 +298,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=58'
+                suffix: '.json?v=59'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
