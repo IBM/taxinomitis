8e1bb7fb5d51a6992384c2621a488e940227392f
Dale Lane <dale.lane@uk.ibm.com>
2023-01-10 17:07:46 +0000

    chore: increase size of "Try it now" class
    I've added additional Watson Assistant API keys to the
    session-users class so that I can increase the number of users
    allowed to use Try it Now concurrently.
    
    This is because I've seen the class limit being hit a few times in
    the last week.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -298,7 +298,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=61'
+                suffix: '.json?v=62'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
