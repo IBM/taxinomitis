ffc21a0f7baa4f8d8013de2331de39d58bc1048e
Dale Lane <dale.lane@uk.ibm.com>
2023-01-11 21:04:07 +0000

    fix: reduce spam emails
    Since the start of this month, I've been receiving hundreds of
    emails a day - all unmodified versions of the "new class account"
    email template, and all from users viewing the site with the
    "ar" language code.
    
    To try and mitigate this, I'm removing the template email for
    these users. This will force them to copy/paste the info from the
    page into their email client - it won't prevent legitimate users
    from contacting me, but it will hopefully reduce at least some of
    the spam I'm getting.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/components/login/login.controller.js
+++ mlforkids-api/public/components/login/login.controller.js
@@ -6,19 +6,30 @@
 
     LoginController.$inject = [
         'authService', 'loggerService', '$location', '$stateParams',
-        '$document', '$scope', '$timeout', '$state',
-        '$mdDialog'
+        '$document', '$scope', '$timeout', '$state', '$translate', '$mdDialog'
     ];
 
-    function LoginController(authService, loggerService, $location, $stateParams, $document, $scope, $timeout, $state, $mdDialog) {
+    function LoginController(authService, loggerService, $location, $stateParams, $document, $scope, $timeout, $state, $translate, $mdDialog) {
         var vm = this;
         vm.authService = authService;
 
         vm.loginstate = 'login';
         readFocusFromUrl();
 
-        vm.sendEmail = function () {
-            window.location = 'mailto:dale.lane@uk.ibm.com?subject=New%20MLforKids%20class%20account&body=___PLEASE%20FILL%20IN%20THIS%20TEMPLATE___%0A%0APlease%20can%20you%20setup%20a%20new%20class%20account%20for%20my%20group.%20%0A%0AI%20need%20it%20for%20my%20group%20of%20___THIS%20MANY___%20students.%20%0A%0AI%20run%20___NAME%20OF%20SCHOOL%20%2F%20DESCRIPTION%20OF%20CODING%20GROUP___.%20%0A%0AYou%20can%20find%20us%20at%20___WEB%20ADDRESS%20FOR%20SCHOOL%20OR%20CODING%20GROUP___.%0A%0AThanks%20very%20much!%0A%0A___WHO%20I%20AM___';
+        vm.sendEmail = function (ev) {
+            var lang = $translate.use();
+            if (lang === 'ar') {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .title('How to ask Dale to create a managed class account for you')
+                        .htmlContent('<div>If you are a teacher or you run a not-for-profit coding group for children, please email me at dale.lane@uk.ibm.com with : <ul><li>your name and title</li><li>the name and web address of your school or coding group</li><li>the number of students in your group</li></ul></div>')
+                        .ok('OK')
+                        .targetEvent(ev)
+                    );
+            }
+            else {
+                window.location = 'mailto:dale.lane@uk.ibm.com?subject=New%20MLforKids%20class%20account&body=___PLEASE%20FILL%20IN%20THIS%20TEMPLATE___%0A%0APlease%20can%20you%20setup%20a%20new%20class%20account%20for%20my%20group.%20%0A%0AI%20need%20it%20for%20my%20group%20of%20___THIS%20MANY___%20students.%20%0A%0AI%20run%20___NAME%20OF%20SCHOOL%20%2F%20DESCRIPTION%20OF%20CODING%20GROUP___.%20%0A%0AYou%20can%20find%20us%20at%20___WEB%20ADDRESS%20FOR%20SCHOOL%20OR%20CODING%20GROUP___.%0A%0AThanks%20very%20much!%0A%0A___WHO%20I%20AM___';
+            }
         };
 
         vm.deployment = $stateParams.DEPLOYMENT;
@@ -48,7 +59,6 @@
                     }, 0);
                 });
         };
-
 
 
 
