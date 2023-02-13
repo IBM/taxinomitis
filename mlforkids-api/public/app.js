715183ef14802613bdc0ea7e5bd6f621a8e3116d
Dale Lane <dale.lane@uk.ibm.com>
2023-02-13 21:06:37 +0000

    chore: Out of office notification
    I'm going camping and won't have Internet access for a week, so
    best to disable the email-me button while I'm away.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -298,7 +298,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=84'
+                suffix: '.json?v=85'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
