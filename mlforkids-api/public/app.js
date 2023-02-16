f2ec1ad6fb441b155128c668de4b34b8906127f9
Dale Lane <dale.lane@uk.ibm.com>
2023-02-16 20:39:58 +0000

    chore: revert Out Of Office notification
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -298,7 +298,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=85'
+                suffix: '.json?v=86'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
