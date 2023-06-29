5545a4793ca1ac58d26d9d70042cb7eadfd0a14e
Dale Lane <dale.lane@uk.ibm.com>
2023-06-29 21:55:20 +0100

    chore: promote new Edublocks page to production
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -304,7 +304,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=104'
+                suffix: '.json?v=105'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
