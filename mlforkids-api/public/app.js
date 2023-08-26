2fd9a66a3f14e36d9ed499b11a7f63f52e7e3642
Dale Lane <dale.lane@uk.ibm.com>
2023-08-26 17:17:36 +0100

    fix: broken image for organic waste data set
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -304,7 +304,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=112'
+                suffix: '.json?v=113'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
