8b7b418e438bb4e9463c0209c50d45240de276be
Dale Lane <dale.lane@uk.ibm.com>
2024-02-19 21:54:46 +0000

    chore: update Sentry
    added some rules from the Sentry docs to reduce the amount of
    noise I get in the issues
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/components/utils/utils.service.js
+++ mlforkids-api/public/components/utils/utils.service.js
@@ -44,7 +44,7 @@
         }
 
         function loadTensorFlow() {
-            return loadScript('/static/bower_components/tfjs/tf.min.js?v=141');
+            return loadScript('/static/bower_components/tfjs/tf.min.js?v=142');
         }
 
 
--- mlforkids-api/public/prod-sentry.js
+++ mlforkids-api/public/prod-sentry.js
@@ -1,6 +1,35 @@
 if (Sentry) {
     Sentry.init({
         dsn: 'https://b4bbe1a8fbce473cb9eb089652848a1e@sentry.io/202347',
-        release: '141'
+        release: '142',
+        // https://docs.sentry.io/platforms/javascript/configuration/filtering/#decluttering-sentry
+        ignoreErrors: [
+            "top.GLOBALS",
+            "originalCreateNotification",
+            "canvas.contentDocument",
+            "MyApp_RemoveAllHighlights",
+            "http://tt.epicplay.com",
+            "Can't find variable: ZiteReader",
+            "jigsaw is not defined",
+            "ComboSearch is not defined",
+            "http://loading.retry.widdit.com/",
+            "atomicFindClose",
+            "fb_xd_fragment",
+            "bmi_SafeAddOnload",
+            "EBCallBackMessageReceived",
+            "conduitPage",
+        ],
+        denyUrls: [
+            /graph\.facebook\.com/i,
+            /connect\.facebook\.net\/en_US\/all\.js/i,
+            /eatdifferent\.com\.woopra-ns\.com/i,
+            /static\.woopra\.com\/js\/woopra\.js/i,
+            /extensions\//i,
+            /^chrome:\/\//i,
+            /^chrome-extension:\/\//i,
+            /127\.0\.0\.1:4001\/isrunning/i,
+            /webappstoolbarba\.texthelp\.com\//i,
+            /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
+        ]
     });
 }
