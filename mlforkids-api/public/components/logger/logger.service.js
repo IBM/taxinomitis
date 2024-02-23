a2ec7a159010ab4af1d452772f195f83ff7d6ca2
Dale Lane <dale.lane@uk.ibm.com>
2024-02-23 15:15:21 +0000

    chore: prepare release
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/components/logger/logger.service.js
+++ mlforkids-api/public/components/logger/logger.service.js
@@ -43,7 +43,7 @@
             logs.push($window.location.href);
             logs.push('\n[ml4klog] url parameters ');
             logs.push(JSON.stringify(urlParms));
-            logs.push('\n[ml4klog] version v=146');
+            logs.push('\n[ml4klog] version v=147');
 
             downloadService.downloadFile(logs, 'text/plain', 'mlforkids.log');
         }
