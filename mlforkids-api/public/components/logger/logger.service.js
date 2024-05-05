0e2286227ddb6c7cca93508fba8a784451c81629
Dale Lane <dale.lane@uk.ibm.com>
2024-05-05 12:01:44 +0100

    chore: prepare new version
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/components/logger/logger.service.js
+++ mlforkids-api/public/components/logger/logger.service.js
@@ -43,7 +43,7 @@
             logs.push($window.location.href);
             logs.push('\n[ml4klog] url parameters ');
             logs.push(JSON.stringify(urlParms));
-            logs.push('\n[ml4klog] version v=181');
+            logs.push('\n[ml4klog] version v=182');
 
             downloadService.downloadFile(logs, 'text/plain', 'mlforkids.log');
         }
--- mlforkids-api/public/components/utils/utils.service.js
+++ mlforkids-api/public/components/utils/utils.service.js
@@ -47,7 +47,7 @@
         }
 
         function loadTensorFlow() {
-            return loadScript('/static/bower_components/tfjs/tf.min.js?v=181');
+            return loadScript('/static/bower_components/tfjs/tf.min.js?v=182');
         }
 
 
