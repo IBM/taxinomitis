2f0c46faddff31e770fcbd1b09805c3b026d20d5
Dale Lane <dale.lane@uk.ibm.com>
2024-05-20 19:30:32 +0100

    chore: version updates for phishing
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -319,7 +319,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=195'
+                suffix: '.json?v=196'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
