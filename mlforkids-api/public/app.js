b0ee9ba0d329b17283188079fdfe680defc10756
Dale Lane <dale.lane@uk.ibm.com>
2024-08-04 10:14:35 +0100

    fix: remove redundant NLS code from Help page
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -319,7 +319,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=205'
+                suffix: '.json?v=206'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
