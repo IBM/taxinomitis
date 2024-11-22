3d1e63d9f8bcd394fc81050c154055a5f368b716
Dale Lane <dale.lane@uk.ibm.com>
2024-11-22 16:46:06 +0000

    feat: identifying scrapers
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -319,7 +319,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=210'
+                suffix: '.json?v=211'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
