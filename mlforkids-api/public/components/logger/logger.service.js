7091648925816e490410479e45b3155ad0f7fe7b
Dale Lane <dale.lane@uk.ibm.com>
2025-02-01 19:55:19 +0000

    chore: upgrade Node.js
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/components/logger/logger.service.js
+++ mlforkids-api/public/components/logger/logger.service.js
@@ -45,7 +45,7 @@
             logs.push(JSON.stringify(urlParms));
             logs.push('\n[ml4klog] browser ');
             logs.push(navigator.userAgent);
-            logs.push('\n[ml4klog] version v=229');
+            logs.push('\n[ml4klog] version v=230');
 
             downloadService.downloadFile(logs, 'text/plain', 'mlforkids.log');
         }
