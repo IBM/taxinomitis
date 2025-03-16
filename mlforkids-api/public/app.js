78327f62edbb9af1ab4db54a2c0d6a9e406ea700
Dale Lane <dale.lane@uk.ibm.com>
2025-03-16 22:09:40 +0000

    chore: update dependencies
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -325,7 +325,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=243'
+                suffix: '.json?v=244'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
--- mlforkids-api/public/components/logger/logger.service.js
+++ mlforkids-api/public/components/logger/logger.service.js
@@ -45,7 +45,7 @@
             logs.push(JSON.stringify(urlParms));
             logs.push('\n[ml4klog] browser ');
             logs.push(navigator.userAgent);
-            logs.push('\n[ml4klog] version v=243');
+            logs.push('\n[ml4klog] version v=244');
 
             downloadService.downloadFile(logs, 'text/plain', 'mlforkids.log');
         }
