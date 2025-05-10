a4fa08f01dca680ea0e06ac847e265f247b69602
Dale Lane <dale.lane@uk.ibm.com>
2025-05-10 12:50:40 +0100

    fix: restore crowd-sourced projects badge
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -327,7 +327,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=251'
+                suffix: '.json?v=252'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
