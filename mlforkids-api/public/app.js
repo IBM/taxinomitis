317741130eb8f953ec75d1a9e0843667d8848f8c
Dale Lane <dale.lane@uk.ibm.com>
2025-07-22 21:45:30 +0100

    chore: out of office
    I'm going on holiday! And (for once) leaving the laptop at home.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -327,7 +327,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=257'
+                suffix: '.json?v=258'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
