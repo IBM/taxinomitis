f2b706e39700d759e8022537edba45fcfb12a00f
Dale Lane <dale.lane@uk.ibm.com>
2025-08-10 22:16:13 +0100

    chore: removing out-of-office
    Changes missed in the previous commit to remove the ooo notice
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -327,7 +327,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=259'
+                suffix: '.json?v=260'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
