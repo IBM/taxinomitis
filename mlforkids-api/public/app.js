fac517aa111239a9d980728ba020ba2bc24db73a
Dale Lane <dale.lane@uk.ibm.com>
2025-12-12 23:18:16 +0000

    chore: prepare new release
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -327,7 +327,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=274'
+                suffix: '.json?v=275'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
