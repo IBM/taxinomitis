d9c16eeb95c863ce8437e92a6b50c4878faebe67
Dale Lane <dale.lane@uk.ibm.com>
2026-01-16 00:29:29 +0000

    fix: improve error message for image training failures
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- mlforkids-api/public/app.js
+++ mlforkids-api/public/app.js
@@ -327,7 +327,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=291'
+                suffix: '.json?v=292'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;
--- mlforkids-api/public/components/logger/logger.service.js
+++ mlforkids-api/public/components/logger/logger.service.js
@@ -45,7 +45,7 @@
             logs.push(JSON.stringify(urlParms));
             logs.push('\n[ml4klog] browser ');
             logs.push(navigator.userAgent);
-            logs.push('\n[ml4klog] version v=291');
+            logs.push('\n[ml4klog] version v=292');
 
             downloadService.downloadFile(logs, 'text/plain', 'mlforkids.log');
         }
