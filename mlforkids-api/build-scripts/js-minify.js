IBM/taxinomitis

commit 64414f0d40d473095eced03018fe479b4d227a74

fix: source maps references to original source not concatenated
Should have been done in 6c6f4e7f1713d28582c072b1ca5d30d437d2708f

Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

mlforkids-api/build-scripts/js-minify.js
@@ -70,18 +70,30 @@ function findJsFiles(dir) {
 
 webJsFiles.push(...findJsFiles(componentsDir));
 
-// Concatenate all JS
-let concatenated = '';
+console.log(`  Processing ${webJsFiles.length} JavaScript files...`);
+
+// Build source map with original file references
+const sources = {};
 for (const file of webJsFiles) {
     if (fs.existsSync(file)) {
-        concatenated += fs.readFileSync(file, 'utf8') + '\n';
+        const relativePath = path.relative(path.join(baseDir, 'web', 'static'), file);
+        sources[relativePath] = fs.readFileSync(file, 'utf8');
     }
 }
 
-console.log(`  Processing ${webJsFiles.length} JavaScript files...`);
+// Concatenate all JS for ng-annotate
+let concatenated = '';
+for (const file of webJsFiles) {
+    if (fs.existsSync(file)) {
+        const content = fs.readFileSync(file, 'utf8');
+        const relativePath = path.relative(path.join(baseDir, 'web', 'static'), file);
+        concatenated += `\n//# sourceURL=${relativePath}\n`;
+        concatenated += content + '\n';
+    }
+}
 
 // Apply ng-annotate
-const annotated = ngAnnotate(concatenated, { add: true });
+const annotated = ngAnnotate(concatenated, { add: true, sourcemap: true });
 if (annotated.errors) {
     console.error('ng-annotate errors:', annotated.errors);
     process.exit(1);
@@ -90,10 +102,11 @@ if (annotated.errors) {
 // Minify with Terser
 (async () => {
     try {
-        const minified = await minify(annotated.src, {
+        const minified = await minify(sources, {
             sourceMap: {
                 filename: 'mlapp.min.js',
-                url: 'mlapp.min.js.map'
+                url: 'mlapp.min.js.map',
+                includeSources: true
             },
             compress: {
                 dead_code: true,

commit 2a05388bacad00bd1292ae48c0557b09c4c166c7

fix: correct ng-annotate application that broke some components
Bug introduced in 64414f0d40d473095eced03018fe479b4d227a74

Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

mlforkids-api/build-scripts/js-minify.js
@@ -72,31 +72,23 @@ webJsFiles.push(...findJsFiles(componentsDir));
 
 console.log(`  Processing ${webJsFiles.length} JavaScript files...`);
 
-// Build source map with original file references
+// Apply ng-annotate to each file individually and build sources object
 const sources = {};
 for (const file of webJsFiles) {
     if (fs.existsSync(file)) {
-        const relativePath = path.relative(path.join(baseDir, 'web', 'static'), file);
-        sources[relativePath] = fs.readFileSync(file, 'utf8');
-    }
-}
-
-// Concatenate all JS for ng-annotate
-let concatenated = '';
-for (const file of webJsFiles) {
-    if (fs.existsSync(file)) {
         const content = fs.readFileSync(file, 'utf8');
-        const relativePath = path.relative(path.join(baseDir, 'web', 'static'), file);
-        concatenated += `\n//# sourceURL=${relativePath}\n`;
-        concatenated += content + '\n';
-    }
-}
 
-// Apply ng-annotate
-const annotated = ngAnnotate(concatenated, { add: true, sourcemap: true });
-if (annotated.errors) {
-    console.error('ng-annotate errors:', annotated.errors);
-    process.exit(1);
+        // Apply ng-annotate to this file
+        const annotated = ngAnnotate(content, { add: true });
+        if (annotated.errors) {
+            console.error(`ng-annotate errors in ${file}:`, annotated.errors);
+            process.exit(1);
+        }
+
+        // Use relative path from web/static for source map
+        const relativePath = path.relative(path.join(baseDir, 'web', 'static'), file);
+        sources[relativePath] = annotated.src;
+    }
 }
 
 // Minify with Terser

mlforkids-api/ops/app.env
@@ -1,5 +1,5 @@
 
-DOCKER_VERSION=298
+DOCKER_VERSION=299
 
 DOCKER_ORG=dalelane
 DOCKER_IMAGE=mlforkids-api

mlforkids-api/public/app.js
@@ -333,7 +333,7 @@
             .useSanitizeValueStrategy('sanitizeParameters')
             .useStaticFilesLoader({
                 prefix: 'static/languages/',
-                suffix: '.json?v=298'
+                suffix: '.json?v=299'
             })
             .determinePreferredLanguage(function () {
                 var lang = navigator.userLanguage || navigator.language;

mlforkids-api/public/components/logger/logger.service.js
@@ -45,7 +45,7 @@
             logs.push(JSON.stringify(urlParms));
             logs.push('\n[ml4klog] browser ');
             logs.push(navigator.userAgent);
-            logs.push('\n[ml4klog] version v=298');
+            logs.push('\n[ml4klog] version v=299');
 
             downloadService.downloadFile(logs, 'text/plain', 'mlforkids.log');
         }

mlforkids-api/public/index.html
@@ -144,10 +144,10 @@
         <script src="https://browser.sentry-cdn.com/10.36.0/bundle.min.js" crossorigin="anonymous"></script>
         <%= AFTER_USE_IN_PROD_ONLY %>
 
-        <script type="text/javascript" src="/static/app.js?v=298"></script>
-        <script type="text/javascript" src="/static/mlapp.min.js?v=298"></script>
+        <script type="text/javascript" src="/static/app.js?v=299"></script>
+        <script type="text/javascript" src="/static/mlapp.min.js?v=299"></script>
 
-        <link href="/static/style.min.css?v=298" rel="stylesheet" />
+        <link href="/static/style.min.css?v=299" rel="stylesheet" />
 
         <link href="/static/bower_components/angular-material/angular-material.theme.min.css?v=3" rel="stylesheet" media="print" onload="this.media='all'; this.onload=null;">
 

mlforkids-api/public/prod-sentry.js
@@ -1,7 +1,7 @@
 if (Sentry) {
     Sentry.init({
         dsn: 'https://b4bbe1a8fbce473cb9eb089652848a1e@sentry.io/202347',
-        release: '298',
+        release: '299',
         // https://docs.sentry.io/platforms/javascript/configuration/filtering/#decluttering-sentry
         ignoreErrors: [
             "top.GLOBALS",