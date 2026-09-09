22ad94f8bcb4e0c2edc139fe1ca9c6cb46fce6b0
Yash Raj <yashraj10messi@gmail.com>
2026-09-10 01:56:44 +0530

    test(utils): fix postgresql required env vars in env unit test suite (#541)
    - Configure POSTGRESQLPORT and POSTGRESQLDATABASE in test harness
    - Prevent false-positive failure on default environment validation

--- mlforkids-api/src/tests/utils/env.ts
+++ mlforkids-api/src/tests/utils/env.ts
@@ -8,26 +8,37 @@ import * as env from '../../lib/utils/env';
 describe('Utils - env', () => {
 
     let oldDBHost: string | undefined;
+    let oldDBPort: string | undefined;
     let oldDBUser: string | undefined;
+    let oldDBDatabase: string | undefined;
 
     before(() => {
         oldDBHost = process.env.POSTGRESQLHOST;
+        oldDBPort = process.env.POSTGRESQLPORT;
         oldDBUser = process.env.POSTGRESQLUSER;
+        oldDBDatabase = process.env.POSTGRESQLDATABASE;
     });
     after(() => {
         process.env.POSTGRESQLHOST = oldDBHost;
+        process.env.POSTGRESQLPORT = oldDBPort;
         process.env.POSTGRESQLUSER = oldDBUser;
+        process.env.POSTGRESQLDATABASE = oldDBDatabase;
     });
 
     it('should pass if variables are present', () => {
-        process.env.POSTGRESQLHOST = 'creds';
-        process.env.POSTGRESQLUSER = 'bucket';
+        process.env.POSTGRESQLHOST = 'localhost';
+        process.env.POSTGRESQLPORT = '5432';
+        process.env.POSTGRESQLUSER = 'postgres';
+        process.env.POSTGRESQLDATABASE = 'mlforkids';
 
         env.confirmRequiredEnvironment();
     });
 
     it('should fail if variables are missing', () => {
+        process.env.POSTGRESQLHOST = 'localhost';
+        process.env.POSTGRESQLPORT = '5432';
         delete process.env.POSTGRESQLUSER;
+        process.env.POSTGRESQLDATABASE = 'mlforkids';
 
         assert.throws(
             () => env.confirmRequiredEnvironment(),
@@ -36,5 +47,6 @@ describe('Utils - env', () => {
     });
 
 });
+
 
 
